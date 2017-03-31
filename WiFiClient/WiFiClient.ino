7869f8495d5612485f8ec643c460c5b67add04c4
Vinicius <viniciusferreirawk@gmail.com>
2017-03-31 14:26:09 -0300

    fix

---
+++ WiFiClient/WiFiClient.ino
@@ -0,0 +1,94 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "w3GET";
+const char* password = "minhavisita";
+
+const char* host = "w3host.no-ip.org";
+const char* streamId   = "....................";
+const char* privateKey = "....................";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+  delay(5000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 8080;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/rtec/admin/api/1.0/logar";
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  //String PostData = "login=viniciusferreirawk@gmail.com&senha=1234";
+  String PostData = "\{ \"login\" : \"viniciusferreirawk@gmail.com\" , \"senha\" : \"1234\" \}";
+  
+  // This will send the request to the server
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Api-Key: 8jxtbt2lzZOfKU2s7xKPRd4ABuC2I3F3" + "\r\n" + 
+               "Content-Type: application/x-www-form-urlencoded" + "\r\n" + 
+               "Connection: no-cache\r\n"+
+               "Content-Length: "+PostData.length()+"\r\n"+"\r\n");
+  client.print(PostData+"\r\n");
+               
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+}
+
